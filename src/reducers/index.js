30bce7bcab36b28bd15d64ec1636bae18b566d07
impl21 <michal@impl21.com>
2016-04-14 18:01:13 +0200

    Added redux-form integration

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
 import { routerReducer } from 'react-router-redux'
+import { reducer as formReducer } from 'redux-form'
 
 const appReducer = combineReducers({
-    routing: routerReducer
+    routing: routerReducer,
+    form: formReducer
 });
 
 export default function resetStateReducer(state, action) {
