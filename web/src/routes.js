025e864fe2392b9103df688d9deae26bc0268eaf
impl21 <michal@impl21.com>
2016-04-17 18:58:03 +0200

    Code cleanup, refactoring, configuration simplification

--- web/src/routes.js
+++ web/src/routes.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { Router, Route, Link } from 'react-router'
 import { syncHistoryWithStore } from 'react-router-redux'
-import App from './containers/App'
-import About from './containers/About'
-import Login from './containers/Login/LoginForm'
+import App from './views/App'
+import About from './views/About'
+import Login from './views/Login'
 
 import history from './history'
 
---
+++ web/src/views/Login.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import LoginForm from '../components/LoginForm'
+export default class Login extends React.Component {
+    render() {
+        func();
+        return(
+            <div>
+                <LoginForm/>
+            </div>
+        )
+    }
+}
