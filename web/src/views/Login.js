b2ac05a069fe0acc6308d5d01a5389c9fab557b4
impl21 <michal@impl21.com>
2016-04-17 19:00:03 +0200

    Removed unused files

--- web/src/views/Login.js
+++ web/src/views/Login.js
@@ -2,7 +2,6 @@ import React from 'react'
 import LoginForm from '../components/LoginForm'
 export default class Login extends React.Component {
     render() {
-        func();
         return(
             <div>
                 <LoginForm/>
