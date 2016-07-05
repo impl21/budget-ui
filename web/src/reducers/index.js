27f0785d814453e1b536ace1ff2d1d4a4d6ff36f
impl21 <michal@impl21.com>
2016-07-05 11:24:28 +0200

    Added props

--- web/src/reducers/index.js
+++ web/src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux'
 import { routerReducer } from 'react-router-redux'
 import { reducer as formReducer } from 'redux-form'
+import expenseReducer from '../views/Expense/reducer';
 
 const appReducer = combineReducers({
     routing: routerReducer,
-    form: formReducer
+    form: formReducer,
+    expenses: expenseReducer
 });
 
 export default function resetStateReducer(state, action) {
