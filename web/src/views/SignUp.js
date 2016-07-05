c17d15de0c091ab77f8bdefc9a0f6948e9621ec2
impl21 <michal@impl21.com>
2016-07-05 11:23:05 +0200

    Added signup

---
+++ web/src/views/SignUp.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+export default class SignUp extends React.Component {
+    render() {
+        return(
+            <div className="uk-container uk-container-center">
+                <div style={{border: '1px solid green', margin: '0 auto'}}><h1>Sign Up</h1></div>
+            </div>
+        )
+    }
+}
